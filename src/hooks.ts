a33aa7386ee257d99376d9a30e1bc9442eb21af6
Anwesh43 <anweshthecool0@gmail.com>
2024-01-05 21:16:08 +0530

    bi-circle-move-div: adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, CSSProperties} from "react";
 
 export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) =>{
     const [scale, setScale] = useState(0)
@@ -38,5 +38,33 @@ export const useDimension = () => {
     }, [])
     return {
         w, h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10
+    const position = 'absolute'
+    const background = 'white'
+    const sf : number = Math.sin(scale * Math.PI)
+    const border = `2px solid indigo`
+    return {
+        parentStyle() : CSSProperties {
+            return {
+                position, 
+                left: `${w / 2}px`,
+                top: `${h / 2}px`
+            }
+        },
+        circleStyle(i : number) : CSSProperties {
+            return {
+                position, 
+                left: `${(w / 2 - size / 2) * (1 - sf) * (1 - 2 * i)}px`,
+                top: `${-(h / 2 - size / 2) * (1 - sf)}px`,
+                width: `${size}px`,
+                height: `${size}px`,
+                border, 
+                borderRadius: '50%'
+            }
+        }
     }
 }
