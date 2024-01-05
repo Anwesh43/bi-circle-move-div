724cce9318a28f1c2ab7fc3060478ad2ad74aedd
Anwesh43 <anweshthecool0@gmail.com>
2024-01-05 22:19:30 +0530

    bi-circle-move-div: using BCMDComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import BCMDComponent from './BCMDComponent';
 function App() {
   return (
     <div className="App">
-      
+      <BCMDComponent/>
     </div>
   );
 }
