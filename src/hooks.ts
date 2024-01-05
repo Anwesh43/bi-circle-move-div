1ee375dd7c5881e97efbf3f7245382d5dee1d606
Anwesh43 <anweshthecool0@gmail.com>
2024-01-05 21:08:47 +0530

    bi-circle-move-div: adding useDimension hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -22,3 +22,21 @@ export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) =>{
         }
     }
 }
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        const listener = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        window.addEventListener('resize', listener, false)
+        return () => {
+            window.removeEventListener('resize', listener, false)
+        } 
+    }, [])
+    return {
+        w, h
+    }
+}
